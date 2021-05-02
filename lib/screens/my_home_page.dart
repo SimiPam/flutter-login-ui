3965d21d007f420e9a934cba508bb02110dd23a6
SimiPam <simipamdachomo@gmail.com>
2021-05-02 02:13:48 +0100

    Lab 4

--- lib/screens/my_home_page.dart
+++ lib/screens/my_home_page.dart
@@ -68,87 +68,87 @@ class _MyHomePageState extends State<MyHomePage> {
         child: SingleChildScrollView(
           child: Padding(
             padding: const EdgeInsets.all(34.0),
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              crossAxisAlignment: CrossAxisAlignment.stretch,
-              children: <Widget>[
-                SizedBox(
-                  height: 60,
-                ),
-                Text(
-                  'Sign In',
-                  style: Theme.of(context).textTheme.headline1,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 80,
-                ),
-                Text(
-                  "WELCOME BACK",
-                  style: Theme.of(context).textTheme.headline4,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 20,
-                ),
-                // InputTexts('Enter your email'),
-                SizedBox(
-                  height: 20.0,
-                ),
-                InputTextField(
-                    hint: "Email Address",
-                    onDiff: (value) {
-                      kCorrectEmail = false;
-                      submit(value, 1);
-                      print(value);
-                    },
-                    checkIcon: (kCorrectEmail)
-                        ? inputCheck(kActiveColor)
-                        : inputCheck(Colors.white)),
-                // checkIcon: (kCorrectEmail) ? kCircleAvatar : kNoCircleAvatar),
-                SizedBox(
-                  height: 20,
-                ),
-                InputTextField(
-                    hint: "Password",
-                    onDiff: (value) {
-                      submit(value, 2);
-                      print(value);
-                    },
-                    checkIcon: (kCorrectPass)
-                        ? inputCheck(kActiveColor)
-                        : inputCheck(Colors.white)),
-                SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: EdgeInsets.symmetric(vertical: 16.0),
-                  child: Material(
-                    color: buttonColor,
-                    borderRadius: BorderRadius.all(Radius.circular(11.0)),
-                    elevation: 5.0,
-                    child: MaterialButton(
-                      onPressed: _buttonChange,
-                      child: Text(
-                        'Sign In',
+            child: Container(
+              margin: EdgeInsets.symmetric(vertical: 60),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: <Widget>[
+                  Text(
+                    'Sign In',
+                    style: Theme.of(context).textTheme.headline1,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 80,
+                  ),
+                  Text(
+                    "WELCOME BACK",
+                    style: Theme.of(context).textTheme.headline4,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  // InputTexts('Enter your email'),
+                  SizedBox(
+                    height: 20.0,
+                  ),
+                  InputTextField(
+                      hint: "Email Address",
+                      onDiff: (value) {
+                        kCorrectEmail = false;
+                        submit(value, 1);
+                        print(value);
+                      },
+                      checkIcon: (kCorrectEmail)
+                          ? inputCheck(kActiveColor)
+                          : inputCheck(Colors.white)),
+                  // checkIcon: (kCorrectEmail) ? kCircleAvatar : kNoCircleAvatar),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  InputTextField(
+                      hint: "Password",
+                      onDiff: (value) {
+                        submit(value, 2);
+                        print(value);
+                      },
+                      checkIcon: (kCorrectPass)
+                          ? inputCheck(kActiveColor)
+                          : inputCheck(Colors.white)),
+                  SizedBox(
+                    height: 20,
+                  ),
+                  Padding(
+                    padding: EdgeInsets.symmetric(vertical: 16.0),
+                    child: Material(
+                      color: buttonColor,
+                      borderRadius: BorderRadius.all(Radius.circular(11.0)),
+                      elevation: 5.0,
+                      child: MaterialButton(
+                        onPressed: _buttonChange,
+                        child: Text(
+                          'Sign In',
+                        ),
                       ),
                     ),
                   ),
-                ),
-                Text(
-                  'Forgot Password?',
-                  style: Theme.of(context).textTheme.headline2,
-                  textAlign: TextAlign.center,
-                ),
-                SizedBox(
-                  height: 10,
-                ),
-                Text(
-                  "Reset Password",
-                  style: Theme.of(context).textTheme.headline3,
-                  textAlign: TextAlign.center,
-                ),
-              ],
+                  Text(
+                    'Forgot Password?',
+                    style: Theme.of(context).textTheme.headline2,
+                    textAlign: TextAlign.center,
+                  ),
+                  SizedBox(
+                    height: 10,
+                  ),
+                  Text(
+                    "Reset Password",
+                    style: Theme.of(context).textTheme.headline3,
+                    textAlign: TextAlign.center,
+                  ),
+                ],
+              ),
             ),
           ),
         ),
